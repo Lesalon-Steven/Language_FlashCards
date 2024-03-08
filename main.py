806b18a80bda6772d30fe92d65dee67d0cef4f93
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-03-09 00:24:53 +0100

    Flipping mechanism

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from tkinter import *
 import random
 import pandas
+import time
 
 BACKGROUND_COLOR = "#B1DDC6"
+random_word = {}
+
+
+def next_word():
+    global random_word,timer
+    window.after_cancel(timer)
+    random_word = new_dict[random.randint(0, len(new_dict) - 1)]
+    canvas.itemconfig(word_title, text="French", fill="black")
+    canvas.itemconfig(word_to_learn, text=random_word["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_card)
+    timer =window.after(3000, func=card_flip)
+
+
+def card_flip():
+    canvas.itemconfig(word_title, text="English", fill="white")
+    canvas.itemconfig(word_to_learn, text=random_word["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_card)
+
 
 window = Tk()
 window.title("Language Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+timer = window.after(3000, func=card_flip)
 
 # with open("data/french_words.csv", "r") as data:
 #     words = data.read()
@@ -15,25 +34,23 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 data = pandas.read_csv("data/french_words.csv")
 new_dict = data.to_dict(orient="records")
+
 # print(new_dict)
-random_word = new_dict[random.randint(0, len(new_dict)-1)]
-
-def next_word():
-    random_word = new_dict[random.randint(0, len(new_dict) - 1)]
-    french_word = random_word["French"]
-    english_word = random_word["English"]
-    canvas.itemconfig(word_to_learn, text=french_word)
-
-    pass
-        # print(word_dict)
 
 
+# print(word_dict)
+
+
+# def card_flip():
+#     back_card = PhotoImage(file="images/card_back.png")
+#     canvas.create_image(400, 263, image=back_card)
 
 
 canvas = Canvas(width=800, height=526)
 front_card = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_card)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+back_card = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_card)
+word_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
 word_to_learn = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -46,5 +63,7 @@ unknown_button.grid(row=1, column=0)
 right_image = PhotoImage(file="images/right.png")
 right_button = Button(image=right_image, highlightthickness=0, command=next_word)
 right_button.grid(row=1, column=1)
+
+next_word()
 
 window.mainloop()
