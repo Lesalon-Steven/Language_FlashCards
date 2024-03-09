d17998de262474b030837a73af28be260f48fe74
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-03-09 20:08:26 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from tkinter import *
 import random
 import pandas
-import time
 
 BACKGROUND_COLOR = "#B1DDC6"
 random_word = {}
 
 
 def next_word():
-    global random_word,timer
+    global random_word, timer
     window.after_cancel(timer)
     random_word = new_dict[random.randint(0, len(new_dict) - 1)]
     canvas.itemconfig(word_title, text="French", fill="black")
     canvas.itemconfig(word_to_learn, text=random_word["French"], fill="black")
     canvas.itemconfig(card_background, image=front_card)
-    timer =window.after(3000, func=card_flip)
+    timer = window.after(3000, func=card_flip)
 
 
 def card_flip():
@@ -28,23 +27,8 @@ window.title("Language Flashcards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 timer = window.after(3000, func=card_flip)
 
-# with open("data/french_words.csv", "r") as data:
-#     words = data.read()
-#     print(words)
-
 data = pandas.read_csv("data/french_words.csv")
 new_dict = data.to_dict(orient="records")
-
-# print(new_dict)
-
-
-# print(word_dict)
-
-
-# def card_flip():
-#     back_card = PhotoImage(file="images/card_back.png")
-#     canvas.create_image(400, 263, image=back_card)
-
 
 canvas = Canvas(width=800, height=526)
 front_card = PhotoImage(file="images/card_front.png")
